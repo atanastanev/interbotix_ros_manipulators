820f551e8ed45d8cf7818b5b8587671466f8f2ae
Atanas Tanev <atanas.tanev@me.com>
2022-12-07 10:11:53 +0100

    modified simple install script

--- interbotix_ros_xsarms/install/amd64/xsarm_amd64_install_simple.sh
+++ interbotix_ros_xsarms/install/amd64/xsarm_amd64_install_simple.sh
@@ -14,20 +14,13 @@ sudo apt -y install python3-pip
 python3 -m pip install modern_robotics
 
 
-cd $ROB_FOLDERS_BASE_DIR
-cd checkout
-cd $ROB_FOLDERS_ACTIVE_ENV
 
 # udev riles
 cd catkin_ws/src/interbotix_ros_core/interbotix_ros_xseries/interbotix_xs_sdk
 sudo cp 99-interbotix-udev.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && sudo udevadm trigger
 
-cd $ROB_FOLDERS_BASE_DIR
-cd checkout
-cd $ROB_FOLDERS_ACTIVE_ENV
-cd catkin_ws
-
+# go to catkin_ws
 rosdep install --from-paths src --ignore-src -r -y
 
 
